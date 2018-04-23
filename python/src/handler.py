fb532c98cb535aa5cd2b1fb07904bde67021c239
tunagami <tunagami@gmail.com>
2018-04-23 10:52:51 +0200

    fooling around with a sample

--- python/src/handler.py
+++ python/src/handler.py
@@ -1,6 +1,14 @@
 '''Lambda Handler'''
 from __future__ import print_function
 import json
+import boto3
+import os
+
+def read(key):
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket=os.environ['S3_BUCKET'], Key=key)
+    return json.loads(obj['Body'].read())
+
 
 def main(event, context):
     '''main method'''
@@ -9,10 +17,11 @@ def main(event, context):
     print('Received context:', context)
 
     # Be excellent to one another.....
+    s3_obj_body = read(event.get('key_name', 'default_key_name'))
 
     return {
         'statusCode': '200',
-        'body': '{"py": "thon"}',
+        'body': s3_obj_body,
         'headers': {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'

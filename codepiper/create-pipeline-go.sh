e66e42b25e56c75b91c053a890cd3aa3c805a2f8
tunagami <tunagami@gmail.com>
2018-04-26 16:02:11 +0200

    cleaning up scripts

--- codepiper/create-pipeline-go.sh
+++ codepiper/create-pipeline-go.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-STACK_NAME=simple-lambda-go
-PIPELINE_CODE_REPO=go-ground
+STACK_NAME=golang-ztsh-piped-stack
+PIPELINE_CODE_REPO=golang-ztsh
 PIPELINE_CODE_BRANCH=master
 CODEBUILD_RUNTIME_VERSION='golang:1.7.3'
+ARTIFACT_BUCKET_NAME=$(aws ssm get-parameter --name "ztsh-source-bucket" --query "Parameter.Value" --output text)
 
 PIPELINE_STACK_NAME="${STACK_NAME}-codepipeline"
 echo "Creating/update ${PIPELINE_STACK_NAME} with code in ${PIPELINE_CODE_REPO}
@@ -16,4 +17,6 @@ aws cloudformation deploy \
         "TargetStackName=${STACK_NAME}" \
         "CodeCommitRepoName=${PIPELINE_CODE_REPO}" \
         "RuntimeVersion=${CODEBUILD_RUNTIME_VERSION}" \
-        "CodeCommitBranch=${PIPELINE_CODE_BRANCH}"
+        "CodeCommitBranch=${PIPELINE_CODE_BRANCH}" \
+        "LambdaSourceS3Bucket=${ARTIFACT_BUCKET_NAME}"
+
--- codepiper/create-pipeline-python-cc.sh
+++ codepiper/create-pipeline-python-cc.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
-STACK_NAME=simple-lambda-py
+STACK_NAME=python-ztsh-piped-stack
 PIPELINE_CODE_REPO=py-ground
 PIPELINE_CODE_BRANCH=master
 CODEBUILD_RUNTIME_VERSION='python:2.7.12'
+ARTIFACT_BUCKET_NAME=$(aws ssm get-parameter --name "ztsh-source-bucket" --query "Parameter.Value" --output text)
 
 PIPELINE_STACK_NAME="${STACK_NAME}-codepipeline"
 echo "Creating/update ${PIPELINE_STACK_NAME} with code in ${PIPELINE_CODE_REPO}
 from branch ${PIPELINE_CODE_BRANCH}."
 
 aws cloudformation deploy \
-    --template-file ./codepipeline.cf.sam.yml \
+    --template-file ./cloudformation.yml \
     --stack-name ${PIPELINE_STACK_NAME} \
     --capabilities CAPABILITY_IAM \
     --parameter-overrides \
         "TargetStackName=${STACK_NAME}" \
         "CodeCommitRepoName=${PIPELINE_CODE_REPO}" \
         "RuntimeVersion=${CODEBUILD_RUNTIME_VERSION}" \
-        "CodeCommitBranch=${PIPELINE_CODE_BRANCH}"
+        "CodeCommitBranch=${PIPELINE_CODE_BRANCH}" \
+        "LambdaSourceS3Bucket=${ARTIFACT_BUCKET_NAME}"
